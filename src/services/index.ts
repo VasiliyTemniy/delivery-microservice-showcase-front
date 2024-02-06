efbe03b914ca8e7e5d30d48809f263e7c854e84d
VasiliyTemniy <flash_er@mail.ru>
2024-02-06 22:58:33 +0300

    aggregate services with reexport index

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+import orderTrackingService from './orderTracking';
+import deliveryMetaService from './deliveryMeta';
+
+export const api = {
+  orderTracking: orderTrackingService,
+  deliveryMeta: deliveryMetaService
+};
