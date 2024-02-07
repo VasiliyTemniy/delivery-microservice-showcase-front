6fef9d4262e95eb1296a9060cd02ac39cdbfaa37
VasiliyTemniy <flash_er@mail.ru>
2024-02-07 12:37:41 +0300

    add SpringPage generic type

--- src/services/types.ts
+++ src/services/types.ts
@@ -1,3 +1,39 @@
+export type APIResponse<T> = {
+  success: boolean
+  content: T;
+  status?: number;
+};
+
+
+export type SpringPage<T> = {
+  content: T[],
+  pageable: {
+    pageNumber: number,
+    pageSize: number,
+    sort: {
+      empty: boolean,
+      sorted: boolean,
+      unsorted: boolean
+    },
+    offset: number,
+    paged: boolean,
+    unpaged: boolean
+  },
+  last: boolean,
+  totalPages: number,
+  totalElements: number,
+  size: number,
+  number: number,
+  sort: {
+    empty: boolean,
+    sorted: boolean,
+    unsorted: boolean
+  },
+  numberOfElements: number,
+  first: boolean,
+  empty: boolean
+};
+
 type MapToDto<T> = {
   -readonly [Property in keyof T]:
   T[Property] extends Date ? string :
